3683435690dd7f56e3ae58656045ccec1e5a0045
michyaraque <michyaraque@gmail.com>
2025-11-05 19:51:17 +0000

    feat: change node connection handles from vertical to horizontal

--- src/components/DialogNode.tsx
+++ src/components/DialogNode.tsx
@@ -205,7 +205,7 @@ function BaseDialogNode({
       {showTargetHandle && (
         <Handle
           type="target"
-          position={Position.Top}
+          position={Position.Left}
           className="w-3! h-3! bg-neutral-700!"
         />
       )}
@@ -407,7 +407,7 @@ function BaseDialogNode({
       {showSourceHandle && (
         <Handle
           type="source"
-          position={Position.Bottom}
+          position={Position.Right}
           className="w-3! h-3! bg-neutral-700!"
         />
       )}
